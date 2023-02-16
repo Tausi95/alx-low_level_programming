9dde2e66367e4f6c8433b0564c126f20e626b706
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-16 23:31:37 +0200

    size of type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main-a program that print size of variables using the printf function.
+*Return: 0(success)
+*/
+
+int main(void)
+
+{
+
+printf("size of char: %lu byte(s)\n", sizeof(char));
+
+printf("size of int: %lu byte(s)\n", sizeof(int));
+
+print("size of int: %lu byte(s)\n", sizeof(long int));
+
+print("size of int: %lu byte(s)\n", sizeof(long long int));
+
+printf("size of float: %lu byte(s)\n", sizeof(float);
+
+return (0);
+
+}

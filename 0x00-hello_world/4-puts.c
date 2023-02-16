ad505e69f062d35fd3f192787ffd4d70a650265b
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-16 21:28:25 +0200

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a line of string using the puts function.
+ *
+ * Return: 0 (success).
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+

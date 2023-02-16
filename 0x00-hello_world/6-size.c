e592b266b91a6cb21937ba6bbb02ef9919af70cf
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-16 23:53:18 +0200

    size revised

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,21 @@ int main(void)
 
 {
 
-printf("size of char: %lu byte(s)\n", sizeof(char));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-printf("size of int: %lu byte(s)\n", sizeof(int));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 
-print("size of int: %lu byte(s)\n", sizeof(long int));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 
-print("size of int: %lu byte(s)\n", sizeof(long long int));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 
-printf("size of float: %lu byte(s)\n", sizeof(float);
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 

a7deb1639bb787ca6ec0d3c5500f494ab3dc905b
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-19 15:21:53 +0200

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - program starts here
+*return: Always 0 (success)
+*/
+
+int main(void)
+
+{
+
+int y;
+
+for (y = 0; y < 10; y++)
+putchar(y + '0');
+
+putchar('\n');
+
+return (0);
+
+}

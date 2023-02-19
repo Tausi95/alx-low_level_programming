855dac2eabc7cbac9ffb89fb76fad7c1df490a18
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-19 14:19:27 +0200

    alphabet in all cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,14 +15,11 @@ char c;
 for (c = 'a'; c <= 'z'; ++c)
 putchar(c);
 
-putchar('\n');
 
-
-for (c = 'A'; c <= 'Z'; ++c)
+for (c = 'A'; c <= 'Z'; ++c) 
 putchar(c);
 
 putchar('\n');
-
 
 return (0);
 

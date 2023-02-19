09af86135d85354ab48f0f34c5a2fee0802495a6
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-19 12:56:51 +0200

    alphabets in lower and uppercases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - Program starts
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+
+putchar('\n');
+
+
+for (c = 'A'; c <= 'Z'; ++c)
+putchar(c);
+
+putchar('\n');
+
+
+return (0);
+
+}

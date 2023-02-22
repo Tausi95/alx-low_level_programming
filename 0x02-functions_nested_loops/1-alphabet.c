457fc1f1ad9ffcb702f695316300b8de736c3eb2
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-22 09:40:59 +0200

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet - print lowercase alphabet followed by newlinw character.
+*/
+
+void print_alphabet(void)
+
+{
+
+char letter = 'a';
+
+while (letter <= 'z')
+
+{
+
+_putchar(letter);
+letter++;
+
+}
+
+_putchar('\n');
+
+}

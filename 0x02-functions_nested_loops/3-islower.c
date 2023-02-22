bbe8920638f88addbac939ef199861ad74d4a620
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-22 14:52:35 +0200

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
 * _islower - checks for lowercase character
 * Return: 1 if c is lowercase otherwise return 0
+*@c: the caracter to check
 */
 
 int _islower(int c)
@@ -11,9 +12,8 @@ int _islower(int c)
 
 
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 	else
 		return (0);
 
 }
-

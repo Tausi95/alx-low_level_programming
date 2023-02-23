5c52376e88fcff9d3d8589922f2f33771c315aa3
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-23 02:38:55 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* add - add two integers and return the result
+*@i: integer for addition
+*@j: integer for addition
+* Return: 0 always
+*/
+
+int add(int i, int j)
+
+{
+
+
+	int result = i + j;
+
+	return (result);
+
+}

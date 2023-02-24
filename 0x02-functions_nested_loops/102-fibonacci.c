a6caf58bbdcc3b3474072f00a3c646af6ac581d9
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-24 14:34:40 +0200

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,15 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2;
 
-	while (i < 50)
+	printf("1");
+
+	while (i < 49)
 	{
-		if (1 == 0)
-		printf("%ld", j);
-		else if (i == 1)
+
 		printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
+
+		k += j;
+		j = k - j;
 		++i;
 	}
 	printf("\n");

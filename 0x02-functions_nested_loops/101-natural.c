95fe82c9f24bf692ac08e941308720b6a9fb4e42
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-24 12:40:47 +0200

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry to the program
+* Return: 0 always
+*/
+
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

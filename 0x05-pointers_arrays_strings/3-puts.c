a913ee3bf070a736e5169e2abe222be3c9d51536
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-02-28 13:22:50 +0200

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 
 /**
 * _puts - prints a string followed by a new line character
+* @str: prints string charachers
 * Return: null
 */
 
@@ -14,4 +15,4 @@ void _puts(char *str)
 		str++;
 	}
 	_putchar('\n');
-} 
+}

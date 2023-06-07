f0fdb6641d956cf437925994b35f10697641ca06
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-07 20:44:51 +0200

    fixed betty documentation

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-int sqrt_function(int i, int j);
+int _sqrt_function(int i, int j);
 
 /**
-* _sqrt_recursion - a function that returns the square root value of a number
+*_sqrt_recursion - a function that returns the square root value of a number
 * @i: the number to be operated on
 * Return: returns the square root of i as an int
 */
@@ -11,22 +11,22 @@ int _sqrt_recursion(int i)
 {
 	if (i < 0)
 		return (-1);
-	return (sqrt_function(i, 0));
+	return (_sqrt_function(i, 0));
 }
 
 /**
- * sqrt_function - recurses to find the natural
+ * _sqrt_function - recurses to find the natural
  * square root of a number
  * @i: number to calculate the sqaure root of
  * @j: iterator
  *
  * Return: the resulting square root
  */
-int sqrt_function(int i, int j)
+int _sqrt_function(int i, int j)
 {
 	if (j * j > i)
 		return (-1);
 	if (j * j == i)
 		return (i);
-	return (sqrt_function(i, j + 1));
+	return (_sqrt_function(i, j + 1));
 }

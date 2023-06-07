5d69e918fcbd052ad5dd94412b0d5ae9a3b66544
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-07 21:04:53 +0200

    corrected seg fault error

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,5 +13,14 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	if (y % 2 == 0)
+	{
+		int temp = _pw_recursion(x, y / 2);
+		return (temp * temp)
+	}
+	else
+	{
+		int temp = _pow_recursion(x, y / 2);
+		return (x * temp * temp);
+	}
 }

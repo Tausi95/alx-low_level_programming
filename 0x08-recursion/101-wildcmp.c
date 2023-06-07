3d0d70e03feb02b91509a701e38e9971d3a0aa6d
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-07 21:25:54 +0200

    fixed a parenthesis on return statement

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,7 @@
 
 /**
  * wildcmp - Compare strings
- * @s1: pointer to string arguments 
+ * @s1: pointer to string arguments
  * @s2: pointer to string argumentS
  * Return: 0
  */
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	}
 	if (*s2 == '*')
-		return (wildcmp(s1 +1, s2) || wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));

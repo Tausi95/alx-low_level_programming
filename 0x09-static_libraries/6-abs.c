3f25f0b054f6fa367bf90d94dde5cc1fca34a3d8
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-09 17:25:04 +0200

    static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _abs - this functions takes an argument and rturns absolute valuE
+* @num: this computes an integer for an abslote value
+* Return: 0 if success otherwise -
+*/
+
+int _abs(int num)
+
+{
+
+	if (num < 0)
+
+	{
+
+		return (-num);
+
+	}
+
+	else
+
+	{
+
+		return (num);
+
+	}
+
+}

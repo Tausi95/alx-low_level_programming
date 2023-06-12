475c70b27ba21fefaddebe46bc6ae5cb9031a7e8
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-12 17:35:24 +0200

    fixed betty documenation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,30 +11,30 @@
 
 char *create_array(unsigned int size, char c)
 {
-        /* Check if size is 0 */
-        char *arr;
-        unsigned int j;
+	/* Check if size is 0 */
+	char *arr;
+	unsigned int j;
 
-        if (size == 0)
-        {
-                return (NULL);
-        }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-        /* Allocate memory to the array arr*/
+	/* Allocate memory to the array arr*/
 
-        arr = malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char));
 
-        /* Check if allocation was successful */
-        if (arr == NULL)
-        {
-                return (NULL);
-        }
+	/* Check if allocation was successful */
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
 
-        /* Initialize the array, arr with the specified char */
-        for (j = 0; j < size; j++)
-        {
-                arr[j] = c;
-        }
+	/* Initialize the array, arr with the specified char */
+	for (j = 0; j < size; j++)
+	{
+		arr[j] = c;
+	}
 
-        return (arr);
+	return (arr);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,28 +9,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *ptr;
-        int i, j, size, t;
+	char *ptr;
+	int i, j, size, t;
 
-        if (s1 == NULL)
-        {
-                s1 = "";
-        }
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
-        for (i = 0; s1[i]; i++)
-                ;
-        for (j = 0; s2[j]; j++)
-                ;
-        size = i + j + 1;
-        ptr = malloc(size * (sizeof(char)));
-        if (ptr == NULL)
-        {
-                return (NULL);
-        }
-        for (t = 0; t < size; t++)
-               t < i ? (ptr[t] = s1[t]) : (ptr[t] = s2[t - i]);
-        return (ptr);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+	size = i + j + 1;
+	ptr = malloc(size * (sizeof(char)));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (t = 0; t < size; t++)
+		t < i ? (ptr[t] = s1[t]) : (ptr[t] = s2[t - i]);
+	return (ptr);
 }

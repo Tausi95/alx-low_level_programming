f7e22d1ae4e8635fd3f7d58a29fe2d72ed996d3e
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-14 01:34:55 +0200

    free memory allocated to a grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid -A function that frees a grid of integers
+ * @grid: The address of the two dimensional grid
+ * @height: Height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int j;
+
+	for (j = 0; j < height; j++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

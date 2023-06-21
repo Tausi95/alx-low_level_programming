6c7ac562d2f70e1c13c3aec7e0ae28931f3ce53a
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-21 16:01:35 +0200

    betty documentation and variable decralation

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,13 @@
  * @name: Pointer to the name string.
  * @f: Pointer to the function that prints the name.
  *
- * Description: This function takes a name string and a function pointer as parameters.
- *              It calls the provided function, passing the name as an argument, to print the name.
+ * Description: This function takes a name string and a function
+ * pointer as parameters.
+ * It calls the provided function, passing the name as an argument,
+ * to print the name.
  */
 
-void print_name(char *name, void(*f)(char*))
+void print_name(char *name, void(*f)(char *))
 {
 	f(name);
 }

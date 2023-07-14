9bdcedd8d288aae8af53608f04ea0cc385cf2dd0
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-07-14 12:42:06 +0200

    fixed bug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,42 +7,42 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        ssize_t total_letters, bytes_written;
-        int file_descriptor;
-        char *buffer;
+	ssize_t total_letters, bytes_written;
+	int file_descriptor;
+	char *buffer;
 
-        if (!filename)
-                return (0);
+	if (!filename)
+		return (0);
 
-        buffer = malloc(sizeof(char) * (letters + 1));
-        if (buffer == NULL)
-                return (0);
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
 
-        file_descriptor = open(filename, O_RDONLY);
-        if (file_descriptor == -1)
-        {
-                free(buffer);
-                return (0);
-        }
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-        total_letters = read(file_descriptor, buffer, sizeof(char) * letters);
-        if (total_letters == -1)
-        {
-                free(buffer);
-                close(file_descriptor);
-                return (0);
-        }
+	total_letters = read(file_descriptor, buffer, sizeof(char) * letters);
+	if (total_letters == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
 
-        bytes_written = write(STDOUT_FILENO, buffer, total_letters);
-        if (bytes_written == -1)
-        {
-                free(buffer);
-                close(file_descriptor);
-                return (0);
-        }
+	bytes_written = write(STDOUT_FILENO, buffer, total_letters);
+	if (bytes_written == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-        free(buffer);
-        close(file_descriptor);
-        return (bytes_written);
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,41 +1,48 @@
 #include "main.h"
-
+/**
+* append_text_to_file - Appends text to a file.
+* @filename: The name of the file to append to.
+* @text_content: The text content to append.
+*
+* Return: 1 on success, -1 on failure.
+*/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-        ssize_t total_bytes;
-        int file_descriptor;
+	ssize_t total_bytes;
+	int file_descriptor;
 
-        if (!filename)
-                return (-1);
+	if (!filename)
+	return (-1);
 
-        file_descriptor = open(filename, O_WRONLY | O_APPEND);
-        if (file_descriptor == -1)
-                return (-1);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
+	return (-1);
 
-        if (text_content)
-        {
-                total_bytes = write(file_descriptor, text_content, _strlen(text_content));
-                if (total_bytes == -1)
-                {
-                        close(file_descriptor);
-                        return (-1);
-                }
-        }
+	if (text_content)
+	{
+		total_bytes = write(file_descriptor, text_content, _strlen(text_content));
+		if (total_bytes == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
 
-        close(file_descriptor);
-        return (1);
+	close(file_descriptor);
+	return (1);
 }
 
 /**
-  * _strlen - length of a string
-  * @s: input char
-  * Return: length of a string
+* _strlen - length of a string
+* @s: input char
+* Return: length of a string
 **/
 int _strlen(char *s)
 {
-        char *ptr = s;
-        while (*ptr)
-                ptr++;
-        return (ptr - s);
+	char *ptr = s;
+
+	while (*ptr)
+		ptr++;
+	return (ptr - s);
 }

2189106b79b757004173fc1214d729704710e249
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-08-25 13:57:07 +0200

    fixed bugs

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files into a dynamic library named liball.so
-
-# Get a list of all .c files in the current directory, excluding those with prefixes
-c_files=$(ls | grep -E '^[0-9]*-.*\.c$')
-
-# Compile each .c file into the dynamic library
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -o liball.so $c_files
-
+gcc *.c -c -fPIC
+gcc *.o -shared -o liball.so
